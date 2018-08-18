f1fc1fcb7874472f0930f897b9609a351139ae45
edgarpf <edgarpeixoto14@gmail.com>
2018-08-18 14:21:30 -0300

    account actions classes request added

--- src/main/java/com/javano/request/accounts/AccountsCreateRequest.java
+++ src/main/java/com/javano/request/accounts/AccountsCreateRequest.java
@@ -11,10 +11,16 @@ import lombok.Setter;
 public class AccountsCreateRequest extends ActionRequest {
 	private String wallet;
 	private String count;
+	private Boolean work;
 	
 	public AccountsCreateRequest(String wallet, String count) {
 	  action = Action.ACCOUNTS_CREATE;
 	  this.wallet = wallet;
 	  this.count = count;
+	}
+	
+	public AccountsCreateRequest(String wallet, String count, Boolean work) {
+		  this(wallet, count);
+		  this.work = work;
 	}
 }
